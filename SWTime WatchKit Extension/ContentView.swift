bae2c4cc7937284893cecbaabad8305545a4ea92
Steven Groves <stevegroves123@gmail.com>
2020-04-12 19:45:30 +0100

    Initial Commit

---
+++ SWTime WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SWTime WatchKit Extension
+//
+//  Created by steve groves on 12/04/2020.
+//  Copyright © 2020 steve groves. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
